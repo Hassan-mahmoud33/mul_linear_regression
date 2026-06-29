8f5ba451da6ea285e23177fd21993f787b3b9489
Hassan <aiengineerhassan7th@gmail.com>
2026-06-29 16:33:10 +0300

    updated multiple regression

--- mul_reg_file.py
+++ mul_reg_file.py
@@ -8,8 +8,7 @@ def delete():
 delete()
 
 
-path = r"C:\Users\B-UNIT\Desktop\ML\codes\datasets\\profit/housing_data_expanded_5000.csv"
-
+path = os.path.join('data' , 'mul_var_lin_reg.csv')
 
 data = pd.read_csv(path , header=None , names = ['size' , 'bedrooms' , 'price'], skipinitialspace=True  )
 
@@ -17,6 +16,8 @@ data = pd.read_csv(path , header=None , names = ['size' , 'bedrooms' , 'price'],
 data['size'] = pd.to_numeric(data['size'] , errors='coerce')
 data['bedrooms'] = pd.to_numeric(data['bedrooms'] , errors='coerce')
 data['price'] = pd.to_numeric(data['price'] , errors='coerce')
+
+
 
 # show the data
 def display():
@@ -37,10 +38,12 @@ def display():
 
 # display()
 
+cols = data.shape[1]
+X_original = data.iloc[ :  , 0 : cols - 1 ]
+y_original = data.iloc[ : , cols - 1 : cols]
 
-X_original = data[['size', 'bedrooms']]
-y_original = data['price']
-
+# print(X_original.head(2))
+# print(y_original.head(2))
 
 
 # predicting
@@ -49,8 +52,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
-# function return train of first variable , test of first variable
-#  train of second variable , test of second variable
 
 x_train , x_test , y_train , y_test = train_test_split(X_original , y_original , test_size=0.2 , random_state=42)
 
@@ -58,9 +59,8 @@ lin_reg = LinearRegression()
 lin_reg.fit( x_train , y_train )
 
 
-# r2_score ( actual values , predicted values )   : <> بتقول هنسيب تنسيق للرقم الي قبلها
-# print(f"Train R* = {r2_score( y_train , lin_reg.predict(x_train)): .4f}")
-# print(f"test R* = {r2_score( y_test , lin_reg.predict(x_test)): .4f}")
+print(f"Train R* = {r2_score( y_train , lin_reg.predict(x_train)): .4f}")
+print(f"test R* = {r2_score( y_test , lin_reg.predict(x_test)): .4f}")
 
 
 #******************************
@@ -72,28 +72,15 @@ line = lin_reg.predict(x_line)
 
 y_pred = lin_reg.predict(x_test)
 
+fig , ax = plt.subplots(figsize = ( 7 , 5))
 
-fig , (ax1 , ax2) = plt.subplots( 1 , 2 ,figsize = ( 10 , 5))
 
-# بيرسم نقط محور اكس يحتوي علي السعر الحقيقي محور واي السعر المتوقع
-ax1.scatter(y_test , y_pred)
-
-# رقم مش مصفوفه لازم نديله رقمين ل اكس بدايه ونهايه  y_test هنا 
-# ونديله رقمين ل واي بدايه ونهايه
-# انا عايز هنا ارسم خط السعر المتوقع ف هستعمل الواي 
-# لان الاكس تحتوي علي الحجم وعدد الغرف
-#  علشان هي تحتوي علي اصغر واكبر سعر موجود عندنا y_test استخدمنا 
-ax1.plot([y_test.min() , y_test.max()], [y_test.min() , y_test.max()] , 'r-')
-ax1.set_xlabel('actual price')
-ax1.set_ylabel('predicted price')
-ax1.set_title('Actual vs predicted')
-
-ax2.scatter(x_test['size'] , y_test , label = 'Trainset')
-ax2.plot( x , line , 'r-' , label = 'regression line')
-ax2.legend(loc = 2)
-ax2.set_xlabel('Size')
-ax2.set_ylabel('Price')
-ax2.set_title('Predicted Price vs  Size')
+ax.scatter(x_test['size'] , y_test , label = 'Trainset')
+ax.plot( x , line , 'r-' , label = 'regression line')
+ax.legend(loc = 2)
+ax.set_xlabel('Size')
+ax.set_ylabel('Price')
+ax.set_title('Predicted Price vs  Size')
 
 
 plt.show()
